3e60eaaa3a0e3781478d33d9a9cb56c3844d8513
Piotr Kwiatkowski <92725331+Pekillen@users.noreply.github.com>
2023-04-01 09:45:37 +0000

    Database connected, routes added

--- app.py
+++ app.py
@@ -1,43 +1,45 @@
 from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
-
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': ' Los Angeles',
-    'salary': 'USD 750000',
-  },
-  {
-    'id': 2,
-    'title': 'Software developer',
-    'location': ' Oslo',
-    'salary': 'USD 120000'
-  },
-  {
-    'id': 3,
-    'title': 'Data Scientist',
-    'location': 'Berlin',
-    'salary': 'USD 190000'
-  },
-  {
-    'id': 4,
-    'title': 'React developer',
-    'location': ' Geneva',
-    'salary': 'USD 90000'
-  },
-]
 
 
 @app.route("/")
 def render():
-  return render_template('home.html', jobs=JOBS, company_name='Python')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
+
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,50 @@
+from sqlalchemy import create_engine, text
+
+db_connection_string = "mysql+pymysql://8yy589ed1juvmrnyblk8:pscale_pw_IcJ0fNE1vICxsk0qzkrIiJr2up7QBWIHW7RFlAg2fWG@aws.connect.psdb.cloud/pythoncareers?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    column_names = result.keys()
+
+    jobs = []
+
+    for row in result.all():
+      jobs.append(dict(zip(column_names, row)))
+
+    return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
+    rows = []
+    column_name = result.keys()
+    for row in result.all():
+      rows.append(dict(zip(column_name, row)))
+    if len(rows) == 0:
+      return None
+    else:
+      return (rows[0])
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+
+    conn.execute(query,
+                 job_id=job_id,
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'])
